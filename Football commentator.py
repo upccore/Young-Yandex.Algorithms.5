cf209851b12a531fd3bf3bf2bee0bd81f93e15fe
upccore <upccore@list.ru>
2024-03-20 10:33:26 +0300

    Football commentator added

---
+++ Football commentator.py
@@ -0,0 +1,11 @@
+g11, g21 = map(int, input().split(':'))
+g12, g22 = map(int, input().split(':'))
+where = int(input())
+if where == 1:
+    score1 = g11 * 100 + g12 * 101
+    score2 = g21 * 101 + g22 * 100
+    print(max(0, (score2 - score1 + 101) // 101))
+else:
+    score1 = g11 * 101 + g12 * 100
+    score2 = g21 * 100 + g22 * 101
+    print(max(0, (score2 - score1 + 100) // 100))
